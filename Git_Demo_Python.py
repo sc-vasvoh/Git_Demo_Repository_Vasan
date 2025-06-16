21e0b99bacc5f6edef84ad1bf5c83d7591f30258
sc-vasvoh <vasan.vohra@scryai.com>
2025-06-16 23:31:58 +0530

    Add a new function sum_vasan

---
+++ Git_Demo_Python.py
@@ -0,0 +1,16 @@
+#A function to add two values
+def sum_vasan(a : int,b : int) -> int:
+    """
+    Return the addition on integers
+
+    Args :
+    - a (int) : the first integer
+    - b (int) : the second integer
+
+    Returns : 
+    - int : the answer of the performed calculation
+
+    """
+    return a+b
+
+print(sum_vasan(5,6))
